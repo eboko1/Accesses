7aeb12b7c29319e701d3ea5d07c3e691df863e42
vitafranc93@gmail.com <vitafranc93@gmail.com>
2022-03-22 19:07:08 +0200

    update 15:58

--- cypress/support/index.js
+++ cypress/support/index.js
@@ -37,7 +37,7 @@ Cypress.on("test:after:run", (test, runnable) => {
         // example to-do app 15/03/22 23:37 -- displays two todo items by default (failed).png  
         //const screenshot =`screenshots/${Cypress.spec.name}/${runnable.parent.title.replaceAll('/', '').replaceAll(':', '')} -- ${test.title} (failed).png`;    
         
-    const screenshot =`screenshots/${Cypress.spec.name}/${runnable.parent.title.replaceAll(/[/]|[:]/g, '')} -- ${test.title} (failed).png`;
-    addContext({ test }, screenshot); 
+    // // const screenshot =`screenshots/${Cypress.spec.name}/${runnable.parent.title.replaceAll(/[/]|[:]/g, '')} -- ${test.title} (failed).png`;
+    // // addContext({ test }, screenshot); 
    // }
 });

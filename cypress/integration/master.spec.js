c4bbc6ac2be78812a455acef8cc49d63b816333d
qa <eboko1@gmail.com>
2022-05-13 14:53:14 +0300

    test 13.05.2022 t15:00

--- cypress/integration/master.spec.js
+++ cypress/integration/master.spec.js
@@ -19,7 +19,7 @@ const laborDetails = new LaborDetails();
 
 
 var date = new Date();
-//const idClient = '1342'
+//const idClient = '13438'
 const idClient =''+date.getDate()+date.getMonth()+date.getMinutes();
 var second = parseInt(date.getSeconds())+10
 var minute = parseInt(date.getMinutes())+10
